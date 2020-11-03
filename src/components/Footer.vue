85c834eb385577e0de66af2024a77c3478ff8334
jmerestain <59800799+jmerestain@users.noreply.github.com>
2020-11-03 17:59:09 +0800

    Initial Commit
    Added Navbar, bottom Footer half

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="bg-hacker-primary">
+        <div class="container mx-auto px-20 flex flex-col">
+            <div class="flex justify-between items-center border-t">
+                <img :src="require('@/assets/img/logo-white.svg')"
+                class="w-32 py-4" />
+                <p class="text-white text-sm font-light tracking-wide">
+                    © 2020 ITECH. All Rights Reserved.
+                </p>
+            </div>
+        </div>
+    </footer>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav id="navbar">
+        <div class="container mx-auto px-10 flex justify-between items-center">
+            <img :src="require('@/assets/img/logo-primary.svg')"
+            class="w-48 py-2" />
+            <a href="#" class="text-lg font-bold uppercase">Login</a>
+        </div>
+    </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Navbar',
+});
+</script>
+
+<style scoped>
+#navbar {
+  background: #F2F4FA;
+}
+</style>
