0542823d1fdd8b6225e61eb29f653194f8733dc6
nellyXinwei <nelsonalbajr@yahoo.com>
2020-11-06 23:53:04 +0800

    update nav and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,74 @@
 <template>
-<footer class="bg-hacker-primary" style="bottom:0;">
+<footer class="bg-hacker-primary bottom-0 text-white">
+  <div class="container mx-auto px-6 pt-10 pb-6">
+    <div class="flex flex-wrap">
+      <div class="w-full md:w-1/5 text-center md:text-left">
+        <h5 class="uppercase mb-6 font-bold">About</h5>
+        <ul class="mb-4">
+          <li class="mt-2">
+            <a href="#" class="hover:underline  ">ITECH</a>
+          </li>
+          <li class="mt-2">
+            <a href="#" class="hover:underline  ">Vision,Mission</a>
+          </li>
+          <li class="mt-2">
+            <a href="#" class="hover:underline  ">Meet the Authors</a>
+          </li>
+        </ul>
+      </div>
+      <div class="w-full md:w-1/5 text-center md:text-left">
+        <h5 class="uppercase mb-6 font-bold">The 3Hs</h5>
+        <ul class="mb-4">
+          <li class="mt-2">
+            <a href="#" class="hover:underline ">Hacker</a>
+          </li>
+          <li class="mt-2">
+            <a href="#" class="hover:underline ">Hipster</a>
+          </li>
+          <li class="mt-2">
+            <a href="#" class="hover:underline ">Hustler</a>
+          </li>
+        </ul>
+      </div>
+      <div class="w-full md:w-1/5 text-center md:text-left">
+        <h5 class="uppercase mb-6 font-bold">Resources</h5>
+        <ul class="mb-4">
+          <li class="mt-2">
+            <a href="#" class="hover:underline  ">Articles</a>
+          </li>
+          <li class="mt-2">
+            <a href="#" class="hover:underline  ">Videos</a>
+          </li>
+          <li class="mt-2">
+            <a href="#" class="hover:underline  ">Open Source</a>
+          </li>
+        </ul>
+      </div>
+      <div class="w-full md:w-1/5 text-center md:text-left">
+        <h5 class="uppercase mb-6 font-bold">Contact</h5>
+        <ul class="mb-4">
+          <li class="mt-2">
+            <a href="#" class="hover:underline  ">Reader Services</a>
+          </li>
+          <li class="mt-2">
+            <a href="#" class="hover:underline  ">Careers</a>
+          </li>
+          <li class="mt-2">
+            <a href="#" class="hover:underline  ">Connect with Us</a>
+          </li>
+        </ul>
+      </div>
+      <div class="w-full md:w-1/5 text-center md:text-left">
+        <h5 class="uppercase mb-6 font-bold">Press</h5>
+        <ul class="mb-4">
+          <li class="mt-2">
+            <a href="#" class="hover:underline  ">Newsletters</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
   <div class="container mx-auto px-20 flex flex-col">
     <div class="flex justify-between items-center border-t">
       <img :src="require('@/assets/img/logo-white.svg')" class="w-32 py-4" />
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,39 @@
 <template>
-<nav id="navbar" class="nav">
-  <div class="container mx-auto px-10 py-5 flex justify-between items-center">
+<nav class="nav flex items-center justify-between flex-wrap bg-white px-40 py-5 shadow-md">
+  <div class="flex items-center flex-no-shrink text-white">
     <g-link class="nav__link" to="/"><img :src="require('@/assets/img/logo-primary.svg')" class="w-40 py-2" /></g-link>
-    <g-link class="nav__link text-lg font-bold uppercase" to="/login/">Login</g-link>
   </div>
+  <div class="block lg:hidden">
+    <button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+      <svg class="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <title>Menu</title>
+        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+      </svg>
+    </button>
+  </div>
+
+  <div class="w-full block flex-grow lg:flex lg:items-center  lg:w-auto">
+    <div class="text-sm lg:flex-grow"></div>
+    <!-- <div>
+      <g-link class="nav__link text-lg font-bold uppercase mx-8" to="/login/">Topics</g-link>
+    </div>
+    <div>
+      <g-link class="nav__link text-lg font-bold uppercase mx-8" to="/login/">Profile</g-link>
+    </div>
+    <div>
+      <g-link class="nav__link text-lg font-bold uppercase mx-8" to="/login/">Calendar</g-link>
+    </div> -->
+    <div>
+      <g-link class="nav__link text-lg font-bold uppercase mx-8" to="/login/">Login</g-link>
+    </div>
+  </div>
+
+
+  <!--
+<div class="container mx-auto px-10 py-5 flex justify-between items-center">
+  <g-link class="nav__link" to="/"><img :src="require('@/assets/img/logo-primary.svg')" class="w-40 py-2" /></g-link>
+  <g-link class="nav__link text-lg font-bold uppercase" to="/login/">Login</g-link>
+</div> -->
 </nav>
 </template>
 
