aa713aabe0fb4daf2f366c7df903edbb3b0fac89
jmerestain <59800799+jmerestain@users.noreply.github.com>
2020-11-07 14:47:16 +0800

    Hacker Slide

--- src/components/index-unauth/section2.vue
+++ src/components/index-unauth/section2.vue
@@ -7,7 +7,52 @@
         <!--Slide 1-->
         <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
         <div class="carousel-item absolute opacity-0" style="height:100vh; background: linear-gradient(283.99deg, #4E6AFA 7.28%, #9298FF 100%);">
-          <div class="block h-full w-full text-white text-5xl text-center flex flex-col">
+          <div class="block h-full w-full text-white text-center flex flex-col">
+            <div class="flex flex-col mt-16">
+              <div class="flex flex-col mb-5">
+                <p class="font-objectivity uppercase text-2xl tracking-wide">
+                  Become a
+                </p>
+                <p class="font-neuemachina uppercase text-5xl tracking-wider">
+                  Hacker
+                </p>
+              </div>
+              <div class="flex mt-10 justify-center flex-wrap">
+                <!-- Column 1 -->
+                <div class="flex flex-col">
+                  <div class="flex">
+                    <div class="t1 small-box bg-white rounded-lg mr-4">
+                      &nbsp;
+                    </div>
+                    <div class="t3 small-box bg-white rounded-lg mr-4">
+                      &nbsp;
+                    </div>
+                  </div>
+                  <div class="t2 long-box bg-white rounded-lg mt-4 mr-4">
+                    &nbsp;
+                  </div>
+                </div>
+                <!-- Column 2 -->
+                <div class="big-box bg-white rounded-lg relative">
+                  <img :src="require('@/assets/img/index-unauth/HACKER mascot.svg')"
+                  class="absolute mascot-1" />
+                </div>
+                <!-- Column 3 -->
+                <div class="flex flex-col">
+                  <div class="flex">
+                    <div class="t2 small-box bg-white rounded-lg ml-4">
+                      &nbsp;
+                    </div>
+                    <div class="t1 small-box bg-white rounded-lg ml-4">
+                      &nbsp;
+                    </div>
+                  </div>
+                  <div class="t3 long-box bg-white rounded-lg mt-4 ml-4">
+                    &nbsp;
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
@@ -51,6 +96,59 @@ export default Vue.extend({
 </script>
 
 <style scoped>
+@font-face {
+  font-family: Objectivity;
+  src: url('../../assets/font/Objectivity-Medium.otf');
+}
+@font-face {
+  font-family: Neue Machina;
+  src: url('../../assets/font/NeueMachina-Ultrabold.woff');
+}
+
+.t1 {
+  filter: brightness(80%);
+  background-image: url('../../assets/img/index-unauth/HACKER BG 1.png');
+  background-size: cover;
+}
+.t2 {
+  filter: brightness(80%);
+  background-image: url('../../assets/img/index-unauth/HACKER BG 2.png');
+  background-size: cover;
+}
+.t3 {
+  filter: brightness(80%);
+  background-image: url('../../assets/img/index-unauth/HACKER BG 5.png');
+  background-size: cover;
+}
+
+.font-objectivity {
+  font-family: Objectivity;
+}
+.font-neuemachina {
+  font-family: Neue Machina;
+}
+
+.big-box {
+  width: 20rem;
+  height: 20rem;
+}
+.small-box {
+  height: 9.5rem;
+  width: 9.5rem;
+}
+.long-box {
+  width: 20rem;
+  height: 9.5rem;
+}
+
+.mascot-1 {
+  left: -50px;
+  bottom: -50px;
+  width: 200px;
+  height: auto;
+  z-index: 10;
+}
+
 .carousel-open:checked+.carousel-item {
   position: static;
   opacity: 100;
