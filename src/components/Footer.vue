cfddf8e1e8e9a4f3846c89ea68a336d805053ad7
nellyXinwei <nelsonalbajr@yahoo.com>
2020-12-08 15:54:44 +0800

    finished first column and added socmed links and icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,16 +2,39 @@
   <footer class="bottom-0 px-32 text-white bg-hacker-primary">
     <div class="grid grid-cols-3 gap-2 py-32 ...">
       <!-- START: FIRST COLUMN -->
-      <div class="flex">
-        <div class="w-1/2">
-          <img :src="require('@/assets/img/logo-white.svg')" class="w-full" />
+      <div class="flex" style="overflow: hidden">
+        <div class="w-1/3">
+          <img :src="require('@/assets/img/BB3-Primary.svg')" class="w-11/12" />
         </div>
-        <div class="w-1/2">
+        <div class="w-2/3">
           <p
-            style="font-size: 72px; font-family: objectivity; font-weight: bold"
+            style="font-size: 6rem; font-family: objectivity; font-weight: bold"
           >
             ITECH
           </p>
+          <div style="margin-top: -10px" class="flex">
+            <a href="https://twitter.com/byteadmu" target="_blank" class="mx-2"
+              ><img :src="require('@/assets/img/icons/Twitter.svg')"
+            /></a>
+            <a
+              href="https://www.facebook.com/byteadmu/"
+              target="_blank"
+              class="mx-2"
+              ><img :src="require('@/assets/img/icons/Facebook.svg')"
+            /></a>
+            <a
+              href="https://www.instagram.com/byteadmu/"
+              target="_blank"
+              class="mx-2"
+              ><img :src="require('@/assets/img/icons/Instagram.svg')"
+            /></a>
+            <a
+              href="https://www.linkedin.com/company/byteadmu/"
+              target="_blank"
+              class="mx-2"
+              ><img :src="require('@/assets/img/icons/LinkedIn.svg')"
+            /></a>
+          </div>
         </div>
       </div>
       <!-- END: FIRST COLUMN -->
@@ -21,7 +44,7 @@
       <div class="grid grid-cols-2 gap-2 ...">
         <div>
           <img
-            :src="require('@/assets/img/logo-withWhiteText.svg')"
+            :src="require('@/assets/img/BB3-PrimaryWithWhiteText.svg')"
             class="w-32 py-6"
           />
           <h5 class="mb-6 font-bold uppercase">
@@ -45,7 +68,7 @@
         <!-- START: SECOND INNER COLUMN -->
         <div>
           <img
-            :src="require('@/assets/img/logo-withWhiteText.svg')"
+            :src="require('@/assets/img/BB3-PrimaryWithWhiteText.svg')"
             class="w-32 py-6"
           />
           <h5 class="mb-6 font-bold uppercase">
@@ -70,7 +93,7 @@
 
       <div>
         <img
-          :src="require('@/assets/img/logo-withWhiteText.svg')"
+          :src="require('@/assets/img/BB3-PrimaryWithWhiteText.svg')"
           style="width: 100%"
         />
       </div>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,58 +1,71 @@
 <template>
-<nav class="nav flex items-center justify-between flex-wrap bg-white px-64 py-5 shadow-md">
-  <div class="flex items-center flex-no-shrink text-white">
-    <g-link class="nav__link" to="/"><img :src="require('@/assets/img/logo-primary.svg')" class="w-40 py-2" /></g-link>
-  </div>
-  <div class="block lg:hidden">
-    <button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
-      <svg class="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-        <title>Menu</title>
-        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-      </svg>
-    </button>
-  </div>
+  <nav
+    class="flex flex-wrap items-center justify-between px-64 py-5 bg-white shadow-md nav"
+  >
+    <div class="flex items-center text-white flex-no-shrink">
+      <g-link class="nav__link" to="/"
+        ><img
+          :src="require('@/assets/img/BB3-PrimaryWithBlackText.svg')"
+          class="w-40 py-2"
+      /></g-link>
+    </div>
+    <div class="block lg:hidden">
+      <button
+        class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white"
+      >
+        <svg
+          class="w-3 h-3"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </button>
+    </div>
 
-  <div class="w-full block flex-grow lg:flex lg:items-center  lg:w-auto">
-    <div class="text-sm lg:flex-grow"></div>
-    <!-- <div>
-      <g-link class="nav__link text-lg font-bold uppercase mx-8" to="/login/">Topics</g-link>
+    <div class="flex-grow block w-full lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow"></div>
+      <!-- <div>
+      <g-link class="mx-8 text-lg font-bold uppercase nav__link" to="/login/">Topics</g-link>
     </div>
     <div>
-      <g-link class="nav__link text-lg font-bold uppercase mx-8" to="/login/">Profile</g-link>
+      <g-link class="mx-8 text-lg font-bold uppercase nav__link" to="/login/">Profile</g-link>
     </div>
     <div>
-      <g-link class="nav__link text-lg font-bold uppercase mx-8" to="/login/">Calendar</g-link>
+      <g-link class="mx-8 text-lg font-bold uppercase nav__link" to="/login/">Calendar</g-link>
     </div> -->
-    <div>
-      <g-link class="nav__link text-lg font-bold uppercase ml-8" to="/login/">Login</g-link>
+      <div>
+        <g-link class="ml-8 text-lg font-bold uppercase nav__link" to="/login/"
+          >Login</g-link
+        >
+      </div>
     </div>
-  </div>
 
-
-  <!--
-<div class="container mx-auto px-10 py-5 flex justify-between items-center">
+    <!--
+<div class="container flex items-center justify-between px-10 py-5 mx-auto">
   <g-link class="nav__link" to="/"><img :src="require('@/assets/img/logo-primary.svg')" class="w-40 py-2" /></g-link>
-  <g-link class="nav__link text-lg font-bold uppercase" to="/login/">Login</g-link>
+  <g-link class="text-lg font-bold uppercase nav__link" to="/login/">Login</g-link>
 </div> -->
-</nav>
+  </nav>
 </template>
 
 <script>
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'Navbar',
+  name: "Navbar",
 });
 </script>
 
 <style scoped>
 @font-face {
   font-family: Objectivity;
-  src: url('../assets/font/Objectivity-Medium.otf');
+  src: url("../assets/font/Objectivity-Medium.otf");
 }
 @font-face {
   font-family: Neue Machina;
-  src: url('../assets/font/NeueMachina-Ultrabold.woff');
+  src: url("../assets/font/NeueMachina-Ultrabold.woff");
 }
 
 nav {
