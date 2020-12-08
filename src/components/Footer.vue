03fa598d9dda29914bee49389b541ce5d8c3c4ce
nellyXinwei <nelsonalbajr@yahoo.com>
2020-12-08 15:19:25 +0800

    footer new layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,89 +1,88 @@
 <template>
-<footer class="bg-hacker-primary bottom-0 px-64 text-white">
-  <div class="pt-10 pb-6">
-    <div class="flex flex-wrap">
-      <div class="w-full md:w-1/5 text-center md:text-left">
-        <h5 class="uppercase mb-6 font-bold">
-          About
-          <hr class="w-10 mt-3" />
-        </h5>
-        <ul class="mb-4">
-          <li class="mt-2">
-            <a href="#" class="hover:underline  ">ITECH</a>
-          </li>
-          <li class="mt-2">
-            <a href="#" class="hover:underline  ">Vision, Mission</a>
-          </li>
-          <li class="mt-2">
-            <a href="#" class="hover:underline  ">Meet the Authors</a>
-          </li>
-        </ul>
+  <footer class="bottom-0 px-32 text-white bg-hacker-primary">
+    <div class="grid grid-cols-3 gap-2 py-32 ...">
+      <!-- START: FIRST COLUMN -->
+      <div class="flex">
+        <div class="w-1/2">
+          <img :src="require('@/assets/img/logo-white.svg')" class="w-full" />
+        </div>
+        <div class="w-1/2">
+          <p
+            style="font-size: 72px; font-family: objectivity; font-weight: bold"
+          >
+            ITECH
+          </p>
+        </div>
       </div>
-      <div class="w-full md:w-1/5 text-center md:text-left">
-        <h5 class="uppercase mb-6 font-bold">The 3Hs
-          <hr class="w-10 mt-3" />
-        </h5>
-        <ul class="mb-4">
-          <li class="mt-2">
-            <a href="#" class="hover:underline ">Hacker</a>
-          </li>
-          <li class="mt-2">
-            <a href="#" class="hover:underline ">Hipster</a>
-          </li>
-          <li class="mt-2">
-            <a href="#" class="hover:underline ">Hustler</a>
-          </li>
-        </ul>
+      <!-- END: FIRST COLUMN -->
+
+      <!-- START: SECOND COLUMN -->
+      <!-- START: FIRST INNER COLUMN -->
+      <div class="grid grid-cols-2 gap-2 ...">
+        <div>
+          <img
+            :src="require('@/assets/img/logo-withWhiteText.svg')"
+            class="w-32 py-6"
+          />
+          <h5 class="mb-6 font-bold uppercase">
+            About
+            <hr class="w-10 mt-3" />
+          </h5>
+          <ul class="mb-4">
+            <li class="mt-2">
+              <a href="#" class="hover:underline">ITECH</a>
+            </li>
+            <li class="mt-2">
+              <a href="#" class="hover:underline">Vision, Mission</a>
+            </li>
+            <li class="mt-2">
+              <a href="#" class="hover:underline">Meet the Authors</a>
+            </li>
+          </ul>
+        </div>
+        <!-- END: FIRST INNER COLUMN -->
+
+        <!-- START: SECOND INNER COLUMN -->
+        <div>
+          <img
+            :src="require('@/assets/img/logo-withWhiteText.svg')"
+            class="w-32 py-6"
+          />
+          <h5 class="mb-6 font-bold uppercase">
+            About
+            <hr class="w-10 mt-3" />
+          </h5>
+          <ul class="mb-4">
+            <li class="mt-2">
+              <a href="#" class="hover:underline">ITECH</a>
+            </li>
+            <li class="mt-2">
+              <a href="#" class="hover:underline">Vision, Mission</a>
+            </li>
+            <li class="mt-2">
+              <a href="#" class="hover:underline">Meet the Authors</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="w-full md:w-1/5 text-center md:text-left">
-        <h5 class="uppercase mb-6 font-bold">Resources
-          <hr class="w-10 mt-3" />
-        </h5>
-        <ul class="mb-4">
-          <li class="mt-2">
-            <a href="#" class="hover:underline  ">Articles</a>
-          </li>
-          <li class="mt-2">
-            <a href="#" class="hover:underline  ">Videos</a>
-          </li>
-          <li class="mt-2">
-            <a href="#" class="hover:underline  ">Open Source</a>
-          </li>
-        </ul>
+      <!-- START: SECOND INNER COLUMN -->
+      <!-- START: SECOND COLUMN -->
+
+      <div>
+        <img
+          :src="require('@/assets/img/logo-withWhiteText.svg')"
+          style="width: 100%"
+        />
       </div>
-      <div class="w-full md:w-1/5 text-center md:text-left">
-        <h5 class="uppercase mb-6 font-bold">Contact
-          <hr class="w-10 mt-3" />
-        </h5>
-        <ul class="mb-4">
-          <li class="mt-2">
-            <a href="#" class="hover:underline  ">Reader Services</a>
-          </li>
-          <li class="mt-2">
-            <a href="#" class="hover:underline  ">Careers</a>
-          </li>
-          <li class="mt-2">
-            <a href="#" class="hover:underline  ">Connect with Us</a>
-          </li>
-        </ul>
-      </div>
-      <div class="w-full md:w-1/5 text-center md:text-left">
-        <h5 class="uppercase mb-6 font-bold">Press
-          <hr class="w-10 mt-3" />
-        </h5>
-        <ul class="mb-4">
-          <li class="mt-2">
-            <a href="#" class="hover:underline  ">Newsletters</a>
-          </li>
-        </ul>
+      <!-- END: THIRD COLUMN -->
+    </div>
+
+    <div class="py-6 pt-10">
+      <div class="items-center justify-center border-t">
+        <p class="my-6 tracking-wide text-center font-extralight">
+          © 2020 ITECH. All Rights Reserved.
+        </p>
       </div>
     </div>
-    <div class="mt-6 flex justify-between items-center border-t">
-      <img :src="require('@/assets/img/logo-withWhiteText.svg')" class="w-32 py-6" />
-      <p class="text-white text-sm font-light tracking-wide">
-        © 2020 ITECH. All Rights Reserved.
-      </p>
-    </div>
-  </div>
-</footer>
+  </footer>
 </template>
