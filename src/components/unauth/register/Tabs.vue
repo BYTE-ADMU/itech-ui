d0d13901e350b52a12b8d853077c93eeba47b0bb
nellyXinwei <nelsonalbajr@yahoo.com>
2020-12-15 01:44:50 +0800

    finished conditional rendering for forms for different tabs of register page

--- src/components/unauth/register/Tabs.vue
+++ src/components/unauth/register/Tabs.vue
@@ -3,30 +3,53 @@
   <div v-bind:tabsData="tabsData" v-for="tabData in tabsData" v-bind:key="tabData.id">
     <Tab>
       <!-- START: BACK BUTTON -->
-      <div  v-if="selectedIndex === 0"><g-link to="/"><button class="float-left" style="position: absolute; left:150px; top:150px; font-size:40px"><</button></g-link></div>
-      <div v-else><button @click='selectTab(selectedIndex-1)' class="float-left" style="position: absolute; left:150px; top:150px; font-size:40px"><</button></div>
+      <div  v-if="selectedIndex === 0"><g-link to="/"><button class="float-left" style="position: absolute; left:250px; top:300px; font-size:40px"><</button></g-link></div>
+      <div v-else><button @click='selectTab(selectedIndex-1)' class="float-left" style="position: absolute; left:250px; top:300px; font-size:40px"><</button></div>
       <!-- END: BACK BUTTON -->
 
-      <div v-if="selectedIndex % 2 === 0">
+      <div v-if="!tabData.isLayoutCentered">
         <div class="flex items-center justify-center h-screen" >
           <div class="flex w-3/4">
-            <div class="w-full mr-20 text-center md:w-1/2 ">
+            <!-- START: IMAGE -->
+            <div class="w-full text-center md:w-1/2 ">
               <img :src="require('@/assets/img/unauth/register/' + tabData.image + '')" class="py-6 mx-auto"/>
             </div>
+            <!-- END: IMAGE -->
 
-            <div class="w-full mt-24 md:w-1/2">
+            <!-- START: FORM -->
+            <div class="w-full mt-24 ml-20 md:w-1/2">
               <h1 class="form_header font-neuemachina">{{tabData.header}}</h1>
               <h1 class="mb-10 form_text font-objectivity">{{tabData.text}}</h1>
 
+              <!-- START: FORM -->
               <div class="form_inputs">
-                <input class="w-full px-8 py-4 mb-10 border rounded-md text-grey-darker" id="username" type="text" placeholder="Kianna Zalameda"/>
+                <div v-if="tabData.id === 1">
+                  <input class="w-full px-8 py-4 mb-10 border rounded-md text-grey-darker" id="username" type="text" placeholder="Kianna Zalameda"/>
+                </div>
+                <div v-else-if="tabData.id === 3">
+                  <input class="w-full px-8 py-4 mb-10 border rounded-md text-grey-darker" id="email" type="text" placeholder="kianna.zalameda@obf.ateneo.edu"/>
+                </div>
+                 <div v-else-if="tabData.id === 5">
+                  <input class="w-full px-8 py-4 mb-10 border rounded-md text-grey-darker" id="password" type="password" placeholder="password"/>
+                  <input class="w-full px-8 py-4 mb-10 border rounded-md text-grey-darker" id="confirm_password" type="password" placeholder="confirm password"/>
+                </div>
+                <div v-else-if="tabData.id === 7">
+                  <input class="w-1/4 px-8 py-4 mb-10 border rounded-md text-grey-darker" id="password" type="text" placeholder="2"/>
+                  <input class="w-3/4 px-8 py-4 mb-10 border rounded-md text-grey-darker" id="confirm_password" type="text" placeholder="Information Technology"/>
+                </div>
+                <div v-else-if="tabData.id === 8">
+                  <input class="w-1/4 px-8 py-4 mb-10 border rounded-md text-grey-darker" id="time" type="text" placeholder="00:00"/>
+                  <input class="w-3/4 px-8 py-4 mb-10 border rounded-md text-grey-darker" id="ampm" type="text" placeholder="AM"/>
+                </div>
               </div>
+              <!-- START: FORM -->
 
               <!-- START: NEXT BUTTON -->
               <div  v-if="selectedIndex === 8"><g-link to="/login/"><button class="px-16 py-4 float-right form_button uppercase font-objectivity ...">{{tabData.buttonText}}</button></g-link></div>
               <div v-else><button class="px-16 py-4 float-right form_button uppercase font-objectivity ..." @click='selectTab(selectedIndex+1)'>{{tabData.buttonText}}</button></div>
               <!-- END: NEXT BUTTON -->
             </div>
+            <!-- END: FORM -->
           </div>
         </div>
       </div>
@@ -35,9 +58,14 @@
         <div class="flex items-center justify-center h-screen" >
           <div class="w-3/4">
             <div class="w-full text-center">
+              <!-- START: TEXT -->
               <h1 class="form_header font-neuemachina" style="margin-top:-120px">{{tabData.header}}</h1>
               <h1 class="w-full mb-10 form_text font-objectivity"><div class="mx-auto" style="max-width:550px">{{tabData.text}}</div></h1>
+              <!-- START: TEXT -->
+
+              <!-- START: IMAGE -->
               <img :src="require('@/assets/img/unauth/register/' + tabData.image + '')" class="py-6 mx-auto "/>
+              <!-- START: IMAGE -->
 
               <!-- START: NEXT BUTTON -->
               <div  v-if="selectedIndex === 8"><g-link to="/login/"><button class="px-16 py-4 mx-auto form_button uppercase font-objectivity ...">{{tabData.buttonText}}</button></g-link></div>
@@ -87,3 +115,5 @@ export default {
   display: none;
 }
 </style>
+
+
--- src/pages/register.vue
+++ src/pages/register.vue
@@ -16,6 +16,7 @@ export default {
       tabsData: [
         {
           id: 1,
+          isLayoutCentered: false,
           image: "Tab1.svg",
           header: "Hi! I am Bit Bot 3,",
           text: "but you can call me BB3! I’d like to know your name!",
@@ -23,6 +24,7 @@ export default {
         },
         {
           id: 2,
+          isLayoutCentered: true,
           image: "Tab2.svg",
           header: "Nice to meet you!",
           text:
@@ -31,6 +33,7 @@ export default {
         },
         {
           id: 3,
+          isLayoutCentered: false,
           image: "Tab3.svg",
           header: "Okay! But first,",
           text: "May I know your OBF email? ",
@@ -38,6 +41,7 @@ export default {
         },
         {
           id: 4,
+          isLayoutCentered: true,
           image: "Tab4.svg",
           header: "Thanks!",
           text:
@@ -46,6 +50,7 @@ export default {
         },
         {
           id: 5,
+          isLayoutCentered: false,
           image: "Tab5.svg",
           header: "Now, an important note",
           text:
@@ -54,6 +59,7 @@ export default {
         },
         {
           id: 6,
+          isLayoutCentered: true,
           image: "Tab6.svg",
           header: "IT’S JUST FOR YOU!",
           text:
@@ -62,6 +68,7 @@ export default {
         },
         {
           id: 7,
+          isLayoutCentered: false,
           image: "Tab7.svg",
           header: "Just curious...",
           text:
@@ -70,6 +77,7 @@ export default {
         },
         {
           id: 8,
+          isLayoutCentered: false,
           image: "Tab8.svg",
           header: "We are almost done!",
           text: "I wanna know what time you want to get reminders to study!",
@@ -77,6 +85,7 @@ export default {
         },
         {
           id: 9,
+          isLayoutCentered: true,
           image: "Tab9.svg",
           header: "We are finally done!",
           text:
