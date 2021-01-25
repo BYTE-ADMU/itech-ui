3bf12c2f8a8dd85f6f8c9d0e2eb5acae6c5735bb
nellyXinwei <nelsonalbajr@yahoo.com>
2021-01-25 16:55:01 +0800

    save changes

---
+++ src/components/auth/topics/cover.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="w-full mb-12">
+    <div class="relative w-full hustlerStyle heighter rounded-xl">
+      <img :src="bitbot" class="bitBot" />
+
+      <div class="flex items-center w-full h-full pl-56">
+        <div class="w-1/3">
+          <h2
+            class="leading-tight text-white select-none text-feature font-neuemachina"
+          >
+            {{ topic.name }}
+          </h2>
+          <p class="text-white font-objectivity">Articles</p>
+        </div>
+        <div class="flex items-center w-2/3 px-40 text-white">
+          <p>{{ topic.description }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "cover",
+  props: ["topic"],
+  computed: {
+    bitbot() {
+      const bitbot = require("../../../assets/img/bitbots/bbwhite.svg");
+      return bitbot;
+    },
+  },
+};
+</script>
+<style scoped >
+.bitBot {
+  position: absolute;
+  width: 189px;
+  height: 193.13px;
+  top: 56px;
+}
+.heighter {
+  height: 204px;
+}
+.hackerStyle {
+  background: linear-gradient(283.99deg, #4e6afa 7.28%, #9298ff 100%);
+}
+.hipsterStyle {
+  background: linear-gradient(283.99deg, #ff7b92 7.28%, #e13894 100%);
+  border-radius: 10px;
+}
+.hustlerStyle {
+  background: linear-gradient(283.99deg, #b0ca88 7.28%, #70b9a2 100%);
+  border-radius: 10px;
+}
+.text-feature {
+  font-size: 79px;
+  line-height: 71px;
+}
+</style>
--- src/templates/topics.vue
+++ src/templates/topics.vue
@@ -1,8 +1,122 @@
-<template></template>
+//START: TEMPLATE
+<template>
+  <Layout>
+    <!-- ROOT -->
+    <div
+      class="container flex flex-col w-screen min-h-screen py-20 mx-auto mb-24"
+    >
+      <div class="flex items-start justify-between w-full">
+        <!-- Featured & New On ITECH -->
+        <div class="flex flex-col w-full">
+          <cover :topic="$page.thisTopic" />
+        </div>
+      </div>
 
+      <hr class="mt-12 mb-6" />
+
+      <h3
+        class="mb-4 text-xl font-bold uppercase font-objectivity"
+        style="color: #9d9d9d"
+      >
+        Related
+      </h3>
+
+      <div class="flex justify-between mt-1 mb-24">
+        <articleEntry
+          v-for="article in filteredArticles"
+          v-bind:key="article.node.id"
+          v-bind:article="article"
+        ></articleEntry>
+      </div>
+    </div>
+  </Layout>
+</template>
+//END: TEMPLATE
+
+// START: PAGE QUERY
+<page-query>
+query($id:ID!){
+  thisTopic: topics(id:$id){
+    id,
+    name,
+    description,
+    courses{
+      id,
+      name,
+    }
+  },
+
+    allArticles(order:DESC){
+      edges{
+        node{
+          publishedDate,
+          id,
+          categories,
+          title,
+          author,
+          thumbnailImage,
+          courses{
+            id,
+            name
+          },
+        }
+      }
+    }    
+  }
+
+
+</page-query>
+// END : PAGE QUERY
+
+//START: SCRIPT
 <script>
-export default {};
+import cover from "../components/auth/topics/cover";
+import articleEntry from "../components/auth/dashboard/articleEntry";
+import playlistTall from "../components/auth/dashboard/playlistTall";
+import bitbotFeature from "../components/auth/dashboard/bitbotFeature";
+import articleHeader from "../components/auth/dashboard/articleHeader";
+
+export default {
+  name: "Courses",
+  metaInfo() {
+    return {
+      title: this.$page.thisCourse.name,
+    };
+  },
+
+  data() {
+    return {
+      articles: [],
+    };
+  },
+
+  mounted() {
+    this.articles = this.$page.allArticles.edges;
+  },
+
+  computed: {
+    filteredArticles() {
+      return this.articles.filter((article) => {
+        return article.node.courses.id.includes(
+          this.$page.thisTopic.courses.id
+        );
+      });
+    },
+  },
+
+  components: {
+    cover,
+    articleEntry,
+    playlistTall,
+    bitbotFeature,
+    articleHeader,
+  },
+};
 </script>
+//END: SCRIPT
 
 <style>
+div > .tg {
+  filter: brightness(80%);
+}
 </style>
