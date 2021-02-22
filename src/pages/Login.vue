8f5f237eef3b2dc255e72782e35631e5b80df1ad
Titia de Castro <laetitia.decastro@obf.ateneo.edu>
2021-02-22 14:16:27 +0800

    fixed login responsiveness, updated graphics

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,10 +1,15 @@
 <template>
   <Layout>
-    <section class="h-screen" style="overflow: hidden">
-      <div class="w-full" style="position: absolute; top: 50%">
-        <div class="flex items-center justify-center">
-          <div class="flex w-3/4" style="margin-top: -120px">
-            <div class="w-full mr-20 text-center md:w-1/2"></div>
+    <section>
+      <!-- <div class="w-full flex">
+        <div class="flex">
+          <div class="flex w-4/5">
+            <div class="w-full text-center md:w-1/2"></div>
+
+            <img
+            :src="require('@/assets/img/unauth/login/Login-Illustration.svg')"
+            class="w-full"
+            />  
 
             <div class="w-full md:w-1/2">
               <h1 class="form_header font-neuemachina">Beep Boop!</h1>
@@ -44,22 +49,75 @@
                   :disabled="!isFormComplete"
                   class="px-16 py-4 form_button uppercase font-objectivity ..."
                 >
-                  Yes I Am
+                  Submit
                 </button>
               </div>
             </div>
           </div>
         </div>
+      </div> -->
+
+      <div class="grid md:flex w-screen p-16 items-center">
+        <img
+            :src="require('@/assets/img/unauth/login/Login-Illustration.svg')"
+            class="ml-6 md:ml-0 md:w-1/2 lg:w-3/5"
+          />  
+
+          <div class="md:w-2/5 lg:mr-16">
+            <div>
+              <h1 class="text-3xl lg:text-4xl font-neuemachina">Beep Boop!</h1>
+              <h1 class="mb-8 md:text-lg lg:text-xl xl:text-2xl font-objectivity">
+                Welcome back ITECH-er! Are you ready to learn more about Hacker.
+                Hipster, and Hustler?
+              </h1>
+            </div>
+
+            <div class="text-md sm:text-xl md:text-lg lg:text-xl xl:text-2xl">
+              <input
+                  class="w-full px-8 py-4 mb-4 border rounded-md text-grey-darker"
+                  id="email"
+                  type="text"
+                  placeholder="email"
+                  v-model="user.email"
+                />
+                <input
+                  class="w-full px-8 py-4 border rounded-md border-red text-grey-darker"
+                  id="password"
+                  type="password"
+                  placeholder="password"
+                  v-model="user.password"
+                />
+            </div>
+
+            <div>
+              <div class="flex justify-between">
+              <g-link
+                class="text-xs sm:text-sm font-objectivity nav__link"
+                to="/register/">
+                <h1 class="my-4 subbutton" style="color: #c0c0c0">
+                  Don't have an account? Register here!
+                </h1>
+              </g-link>
+
+              <g-link
+                class="text-xs sm:text-sm text-right font-objectivity nav__link"
+                to="/forgotpassword/"
+              >
+                <h1 class="my-4 subbutton" style="color: #c0c0c0">
+                  forgot password?
+                </h1>
+              </g-link>
+              </div>
+                <button
+                  @click="login"
+                  :disabled="!isFormComplete"
+                  class="block mx-auto md:float-right px-8 py-3 form_button uppercase font-objectivity ..."
+                >
+                  Submit
+                </button>
+            </div>
+          </div>
       </div>
-      <img
-        :src="require('@/assets/img/unauth/login/waves.svg')"
-        class="w-full"
-        style="position: absolute"
-      />
-      <img
-        :src="require('@/assets/img/unauth/login/loginBG.svg')"
-        class="w-full"
-      />
     </section>
   </Layout>
 </template>
