d001317ef456077ff8394c371db7512a3ce40c4c
Titia de Castro <laetitia.decastro@obf.ateneo.edu>
2021-02-23 13:09:08 +0800

    changed graphics and updated responsiveness

--- src/pages/forgotpassword.vue
+++ src/pages/forgotpassword.vue
@@ -1,54 +1,46 @@
 <template>
   <Layout>
-    <section class="h-screen" style="overflow-y: hidden">
-      <div class="w-full" style="position: absolute; top: 50%">
-        <div class="flex items-center justify-center">
-          <div class="flex w-3/4" style="margin-top: -180px">
-            <div class="w-full mr-20 text-center md:w-1/2">
-              <img
-                :src="require('@/assets/img/unauth/forgotpassword/image.svg')"
-                class="mx-auto"
-              />
-            </div>
+    <section>
+      <div class="grid md:flex w-screen pt-0 pb-10 md:py-10 lg:pb-10 px-8 sm:px-12 lg:px-16 items-center">
+        <img
+          :src="require('@/assets/img/unauth/forgotpassword/ForgotPasswordIllustration.svg')"
+          class="block mx-auto mb-16 md:ml-0 w-3/4 md:w-1/2 lg:w-3/5"
+        />
 
-            <div class="w-full mt-20 md:w-1/2">
-              <h1 class="form_header font-neuemachina">Coming right up!</h1>
-              <h1 class="mb-10 form_text font-objectivity">
-                BB3 got your back! All you have to do is fill up your new
-                password and confirm to get started
-              </h1>
-
-              <div class="form_inputs">
-                <input
-                  class="w-full px-8 py-4 mb-4 border rounded-md text-grey-darker"
-                  id="old_password"
-                  type="paddword"
-                  placeholder="old password"
-                  v-model="user.oldPassword"
-                />
-                <input
-                  class="w-full px-8 py-4 mb-3 mb-6 border rounded-md border-red text-grey-darker"
-                  id="new_password"
-                  type="password"
-                  placeholder="new password"
-                  v-model="user.newPassword"
-                />
-              </div>
-
-              <button
-                :disabled="!isFormComplete"
-                class="px-16 py-4 float-right form_button uppercase font-objectivity ..."
-              >
-                Ready!
-              </button>
-            </div>
+        <div class="md:w-2/5 lg:mr-16">
+          <div>
+            <h1 class="text-3xl lg:text-4xl font-neuemachina">Coming right up!</h1>
+            <h1 class="mb-8 md:text-lg lg:text-xl xl:text-2xl font-objectivity">
+              BB3 got your back! All you have to do is fill up your new
+              password and confirm to get started
+            </h1>
           </div>
+
+          <div class="text-md sm:text-xl md:text-lg lg:text-xl xl:text-2xl">
+            <input
+              class="w-full px-8 py-4 mb-4 border rounded-md text-grey-darker"
+              id="old_password"
+              type="paddword"
+              placeholder="old password"
+              v-model="user.oldPassword"
+            />
+            <input
+              class="w-full px-8 py-4 mb-10 border rounded-md border-red text-grey-darker"
+              id="new_password"
+              type="password"
+              placeholder="new password"
+              v-model="user.newPassword"
+            />
+          </div>
+
+          <button
+            :disabled="!isFormComplete"
+            class="block mx-auto md:float-right px-8 py-3 form_button uppercase font-objectivity ..."
+          >
+            Ready!
+          </button>
         </div>
       </div>
-      <img
-        :src="require('@/assets/img/unauth/forgotpassword/fwaves.svg')"
-        class="w-full absolute"
-      />
     </section>
   </Layout>
 </template>
