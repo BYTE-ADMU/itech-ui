828546c65e9e862c5e3c63c19dd31f1889211b81
nellyXinwei <nelsonalbajr@yahoo.com>
2021-03-03 03:14:47 +0800

    did section 1 and section 2

--- src/components/unauth/index/section1.vue
+++ src/components/unauth/index/section1.vue
@@ -1,38 +1,56 @@
 <template>
-  <section class="flex items-center justify-center h-screen">
-    <div class="flex w-3/4">
-      <div class="w-full text-center md:w-1/2">
-        <img
-          :src="require('@/assets/img/unauth/index/section1-botWelcome.svg')"
-          class="py-6 mx-auto"
-        />
-      </div>
-      <div class="w-full text-center md:w-1/2">
-        <div class="py-40">
-          <h1 class="font-objectivity index_text">Hello! Welcome to</h1>
-          <h1 class="font-neuemachina index_header">ITECH</h1>
+  <div
+    class="grid items-center w-screen min-h-screen grid-cols-1 px-0 pb-16 lg:gap-8 lg:grid-cols-2 md:px-32 lg:px-0"
+    style="background: linear-gradient(180deg, #6b83ff 53.75%, #b8c3fd 91.98%)"
+  >
+    <g-image
+      :src="require('@/assets/img/unauth/index/section1-botWelcome.svg')"
+      class="block w-full mx-auto lg:hidden"
+    />
 
-          <g-link to="/login/"
-            ><button
-              class="font-objectivity rounded-md index_button uppercase ..."
-            >
-              Get Started
-            </button>
-          </g-link>
-          <a
-            class="text-lg font-objectivity nav__link"
-            href="https://tinyurl.com/ITECHPreRegistrationForm"
-            target="_blank"
-          >
-            <h1 class="mt-4 index_subbutton">
-              Don't have credentials yet?<strong> Click</strong> here
-            </h1>
-          </a>
-        </div>
+    <div class="mx-auto md:w-2/3">
+      <h1
+        class="mx-auto text-center text-white bg-red font-objectivity md:w-full index_text"
+      >
+        Hello! Welcome to
+      </h1>
+      <h1
+        class="text-center text-white font-neuemachina md:mx-auto index_header"
+      >
+        ITECH
+      </h1>
+
+      <div>
+        <g-link
+          to="/login/"
+          class="mb-8 block mx-auto uppercase md:w-full float-left md:float-none px-8 py-3 form_button font-objectivity ..."
+        >
+          Get Started
+        </g-link>
+        <a
+          class="text-lg text-center font-objectivity nav__link"
+          href="https://tinyurl.com/ITECHPreRegistrationForm"
+          target="_blank"
+        >
+          <h1 class="hidden mx-auto mt-4 text-white md:block">
+            Don't have credentials yet?<strong> Click</strong> here
+          </h1>
+        </a>
       </div>
     </div>
-  </section>
+    <g-image
+      :src="require('@/assets/img/unauth/index/section1-botWelcome.svg')"
+      class="hidden w-full mx-auto lg:block"
+    />
+  </div>
 </template>
+
+<script>
+export default {
+  name: "unauth-index-section2",
+};
+</script>
+
 
 <script>
 import Vue from "vue";
@@ -53,27 +71,5 @@ export default Vue.extend({
   font-style: normal;
   font-weight: 900;
   font-size: 110px;
-}
-
-.index_button {
-  width: 413px;
-  height: 40px;
-  left: 847px;
-  top: 458px;
-  background: #64c0c1;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 18px;
-  text-align: center;
-  letter-spacing: 0.2em;
-  color: #f9f7f2;
-}
-
-.index_subbutton {
-  font-style: normal;
-  font-weight: normal;
-  font-size: 18px;
-  color: #64c0c1;
-  text-align: center;
 }
 </style>
--- src/components/unauth/index/section3.vue
+++ src/components/unauth/index/section3.vue
@@ -1,83 +1,150 @@
 <template>
-  <section class="flex items-center justify-center h-screen">
-    <div class="flex w-3/4">
-      <div class="w-full md:w-1/2">
-        <div class="p-6 thirdColumnCard">
-          <p class="my-2 header">Level up, Bits!</p>
-          <p class="my-2 text">
-            These articles will not only help you learn and grow your skills,
-            but will also help you earn some points! Move on to the next level
-            by obtaining 5000 points!
-          </p>
-        </div>
-      </div>
-      <div class="w-full text-center md:w-1/2">
-        <img
-          :src="require('@/assets/img/unauth/index/section3.svg')"
-          class="py-6 mx-auto"
-        />
-      </div>
-    </div>
+  <section id="section2" class="">
+    <Slides v-bind:slides="slides" />
   </section>
 </template>
 
 <script>
 import Vue from "vue";
+import Slides from "./section2-slides/Slides.vue";
 
 export default Vue.extend({
   name: "unauth-index-section3",
+
+  components: {
+    Slides,
+  },
+
+  // Data for Slides
+  data() {
+    return {
+      slides: [
+        {
+          id: 0,
+          slideOrder: "carousel-1",
+          becomeWhat: "Hacker",
+          classes: [
+            "slide1", //BG Color
+            "t1",
+            "t2",
+            "t3",
+          ],
+        },
+        {
+          id: 1,
+          slideOrder: "carousel-2",
+          becomeWhat: "Hipster",
+          classes: [
+            "slide2", //BG Color
+            "t3",
+            "t1",
+            "t2",
+          ],
+        },
+        {
+          id: 2,
+          slideOrder: "carousel-3",
+          becomeWhat: "Hustler",
+          classes: [
+            "slide3", //BG Color
+            "t2",
+            "t3",
+            "t1",
+          ],
+        },
+      ],
+    };
+  },
 });
 </script>
 
-<style scoped>
-.hello {
-  font-family: Objectivity;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 40px;
+<style>
+.slide1 {
+  background: linear-gradient(283.99deg, #4e6afa 7.28%, #9298ff 100%);
 }
 
-.itech {
-  font-family: Neue Machina;
-  font-style: normal;
-  font-weight: 900;
-  font-size: 110px;
+.slide2 {
+  background: linear-gradient(283.99deg, #ff7b92 7.28%, #e13894 100%);
 }
 
-.getStarted {
-  width: 413px;
-  height: 40px;
-  left: 847px;
-  top: 458px;
-  background: #64c0c1;
-  border-radius: 50px;
-  font-family: Objectivity;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 18px;
+.slide3 {
+  background: linear-gradient(283.99deg, #b0ca88 7.28%, #70b9a2 100%);
+}
+
+.t1 {
+  filter: brightness(80%);
+  background-image: url("../../../assets/img/unauth/index/section2/slides/HACKER BG 1.png");
+  background-size: cover;
+}
+
+.t2 {
+  filter: brightness(80%);
+  background-image: url("../../../assets/img/unauth/index/section2/slides/HACKER BG 2.png");
+  background-size: cover;
+}
+
+.t3 {
+  filter: brightness(80%);
+  background-image: url("../../../assets/img/unauth/index/section2/slides/HACKER BG 3.png");
+  background-size: cover;
+}
+
+.big-box {
+  width: 20rem;
+  height: 20rem;
+}
+
+.small-box {
+  height: 9.5rem;
+  width: 9.5rem;
+}
+
+.long-box {
+  width: 20rem;
+  height: 9.5rem;
+}
+
+.mascot-1 {
+  left: -50px;
+  bottom: -50px;
+  width: 200px;
+  height: auto;
+  z-index: 10;
+}
+
+.carousel-open:checked + .carousel-item {
+  position: static;
+  opacity: 100;
+}
+
+.carousel-indicators {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  bottom: 10%;
+  left: 0;
+  right: 0;
   text-align: center;
-  letter-spacing: 0.2em;
-  color: #f9f7f2;
+  z-index: 10;
 }
 
-.orLogIn {
-  font-family: Objectivity;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 18px;
-  color: #64c0c1;
-  text-align: center;
-}
-
-.header {
-  font-family: Neue Machina;
-  font-weight: 800;
-  font-size: 56px;
-  color: #333333;
-}
-
-.text {
-  font-size: 32px;
-  color: #333333;
+#carousel-1:checked
+  ~ .control-1
+  ~ .carousel-indicators
+  li:nth-child(1)
+  .carousel-bullet,
+#carousel-2:checked
+  ~ .control-2
+  ~ .carousel-indicators
+  li:nth-child(2)
+  .carousel-bullet,
+#carousel-3:checked
+  ~ .control-3
+  ~ .carousel-indicators
+  li:nth-child(3)
+  .carousel-bullet {
+  color: #ffffff;
+  /*Set to match the Tailwind colour you want the active one to be */
 }
 </style>
