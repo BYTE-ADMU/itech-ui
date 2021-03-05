bba9d3e310217dcf4eb6b145e46fa0b437d4e9a7
nellyXinwei <nelsonalbajr@yahoo.com>
2021-03-05 12:29:36 +0800

    finished landing page

--- src/components/unauth/index/section3.vue
+++ src/components/unauth/index/section3.vue
@@ -6,7 +6,7 @@
 
 <script>
 import Vue from "vue";
-import Slides from "./section2-slides/Slides.vue";
+import Slides from "./section2-slides/HardSlides.vue";
 
 export default Vue.extend({
   name: "unauth-index-section3",
