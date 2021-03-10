9c0702ac98fa93ea95a75aad29f9dcd3bc21a8ea
Titia de Castro <laetitia.decastro@obf.ateneo.edu>
2021-03-11 03:15:40 +0800

    discover filter buttons & footer button hover animation

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -137,12 +137,25 @@
 }
 
 .getStarted {
-  background: #ffffff;
+  /* background: #ffffff; */
   border-radius: 50px;
   font-family: Objectivity;
   font-weight: 700;
   font-size: 16px;
   color: #6b83ff;
+  border: 3px solid #ffffff;
+  background-image: -webkit-linear-gradient(30deg, #ffffff 50%, transparent 50%);
+  background-image: linear-gradient(30deg, #ffffff 50%, transparent 50%);
+  background-size: 1000px;
+  background-repeat: no-repeat;
+  background-position: 0%;
+  -webkit-transition: background 400ms ease-in-out;
+  transition: background 400ms ease-in-out;
+}
+
+.getStarted:hover {
+  background-position: 100%;
+  color: #ffffff;
 }
 .itechFooter{
   margin-top: -20px;
