3573a78162785e36ccd480346995a9528b70579a
Titia de Castro <laetitia.decastro@obf.ateneo.edu>
2021-03-11 00:16:34 +0800

    404 page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,15 +1,39 @@
 <template>
   <div>
-    hello
+    <div class="flex sm:justify-center sm:mx-20">
+      <g-image src="../assets/img/404.svg" class="notFoundDesktop"></g-image>
+      <g-image src="../assets/img/404mobile.svg" class="notFoundMobile w-full"></g-image>
+
+    </div>
+
+    <g-link to="/dashboard" class="w-auto">
+      <button class="block mx-auto mt-10 px-8 pb-3 pt-4 form_button uppercase font-objectivity ...">
+        Back To Home
+      </button>
+    </g-link>
   </div>
 </template>
 
 <script>
 export default {
-
+  metaInfo: {
+    title: "Not Found",
+  },
 }
 </script>
 
-<style>
+<style scoped>
+.notFoundMobile {
+  display: none;
+}
 
+@media screen and (max-width: 640px) {
+  .notFoundMobile {
+    display: block;
+  }
+
+  .notFoundDesktop {
+    display: none;
+  }
+}
 </style>
