f736d7c016a9cebd39380eb93b16fb1ff32e2005
nellyXinwei <nelsonalbajr@yahoo.com>
2021-03-13 01:22:52 +0800

    Create About.vue

---
+++ src/pages/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout> About Page </Layout>
+</template>
+
+<script>
+export default {
+  name: "About",
+  metaInfo: {
+    title: "About",
+  },
+
+  components: {},
+
+  async mounted() {
+    await this.$store.dispatch("articlesStore/getArticles");
+    await this.$store.dispatch("coursesStore/getCourses");
+    await this.$store.dispatch("topicsStore/getTopics");
+    await this.$store.dispatch("categoriesStore/getCategories");
+  },
+};
+</script>
+
+
