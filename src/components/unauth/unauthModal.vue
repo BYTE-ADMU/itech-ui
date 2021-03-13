6e418f95f519b71d25ef980ee26be77882bdef5d
Titia de Castro <laetitia.decastro@obf.ateneo.edu>
2021-03-13 14:35:18 +0800

    no access modal close button leads to login

--- src/components/unauth/unauthModal.vue
+++ src/components/unauth/unauthModal.vue
@@ -2,9 +2,11 @@
   <div class="h-screen w-screen fixed table" style="margin-top: -105px">
     <div class="bg-modal text-center table-cell align-middle">
       <div class="bg-white mx-auto border border-white rounded-xl py-16 relative" style="width:57%">
-        <g-image
-          :src="require('@/assets/img/unauth/close-modal-vector.svg')"
-          class="absolute" style="right: 23px; top: 23px"/>
+        <g-link to="/login/" class="w-full">
+          <g-image
+            :src="require('@/assets/img/unauth/close-modal-vector.svg')"
+            class="absolute" style="right: 23px; top: 23px"/>
+        </g-link>
         <h1 class="font-neuemachina text-4xl mb-12">
           Oops! You don't have access!
         </h1>
