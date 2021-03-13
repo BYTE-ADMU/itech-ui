83106d738e4a9f1e9987ce0a5b412105f2291aa7
nellyXinwei <nelsonalbajr@yahoo.com>
2021-03-13 11:58:22 +0800

    finished section 2

--- src/components/unauth/about/Section2.vue
+++ src/components/unauth/about/Section2.vue
@@ -1,12 +1,38 @@
 <template>
-  <div>Section 2</div>
+  <div class="py-20">
+    <p class="mb-20 byte_description">
+      Members can find introductory courses about
+      <br />
+      topics that fall under the:
+    </p>
+
+    <div class="grid grid-cols-3 gap-5">
+      <Track category="Hacker" />
+      <Track category="Hipster" />
+      <Track category="Hustler" />
+    </div>
+  </div>
 </template>
 
 <script>
+import Track from "./Track";
 export default {
   name: "About-Section2",
+
+  components: {
+    Track,
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.byte_description {
+  font-family: Objectivity;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 25px;
+  text-align: center;
+  color: #151316;
+}
 </style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -29,13 +29,6 @@ export default {
     Section3,
     Section4,
   },
-
-  async mounted() {
-    await this.$store.dispatch("articlesStore/getArticles");
-    await this.$store.dispatch("coursesStore/getCourses");
-    await this.$store.dispatch("topicsStore/getTopics");
-    await this.$store.dispatch("categoriesStore/getCategories");
-  },
 };
 </script>
 
