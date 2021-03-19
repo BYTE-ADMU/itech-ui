07f364c76eb84a67c1bfb35943974ff968d846a1
Titia de Castro <laetitia.decastro@obf.ateneo.edu>
2021-03-20 02:56:42 +0800

    meet the team desktop cover, incomplete mobile cover

---
+++ src/components/unauth/meetTheTeam/coverMobile.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-screen h-screen meet-bg relative">
+    <g-image src="../../../assets/img/unauth/meetTheTeam/top-confetti.png" class="absolute w-screen top-confetti z-0"/>
+    <g-image src="../../../assets/img/unauth/meetTheTeam/bottom-confetti.png" class="absolute w-screen bottom-confetti z-0"/>
+
+    <h1 class="font-neuemachina text-white font-extrabold text-5xl text-center px-8 z-20 relative">Meet the Team</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "coverMobile",
+}
+</script>
+
+<style>
+.meet-bg {
+  background: #64C0C1;
+}
+
+.top-confetti {
+  top: 0;
+}
+
+.bottom-confetti {
+  bottom: 0;
+}
+</style>
--- src/pages/MeetTheTeam.vue
+++ src/pages/MeetTheTeam.vue
@@ -1,15 +1,24 @@
 <template>
   <Layout>
-    
+    <cover class="hidden lg:block"/>
+    <coverMobile class="block lg:hidden"/>
   </Layout>
 </template>
 
 <script>
+import cover from '../components/unauth/meetTheTeam/cover'
+import coverMobile from '../components/unauth/meetTheTeam/coverMobile'
+
 export default {
   name: "MeetTheTeam",
   metaInfo: {
     title: "Meet The Team",
-  }
+  },
+  
+  components: {
+    cover,
+    coverMobile,
+  },
 }
 </script>
 
