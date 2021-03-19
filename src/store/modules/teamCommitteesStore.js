0e3bffbc899455f1f7572650d57ad360484e19a8
Titia de Castro <laetitia.decastro@obf.ateneo.edu>
2021-03-20 01:25:26 +0800

    created teamCommitteesStore.js

---
+++ src/store/modules/teamCommitteesStore.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const teamCommitteesStore = {
+  namespaced: true,
+
+  // to handle state
+  state: () => ({
+    API_URL: 'https://calm-everglades-39473.herokuapp.com',
+    SORT: '?_sort=published_at',
+    teamCommittees: []
+  }),
+
+  // to handle state
+  getters: {
+    allTeamCommittees: (state) => state.teamCommittees,
+  },
+
+  //to handle actions
+  actions: {
+    getTeamCommiittees({ state, commit }) {
+      axios.get(`${state.API_URL}/team-committees${state.SORT}`).then(response => {
+        commit('SET_TEAMCOMMITTEES', response.data);
+        return response.data;
+      })
+    }
+  },
+
+  // to handle mutations
+  mutations: {
+    SSET_TEAMCOMMITTEES: (state, teamCommittees) => state.teamCommittees = teamCommittees,
+  }
+}
+
+export default teamCommitteesStore;
