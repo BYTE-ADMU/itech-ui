c62424a42da6ca65741c14037338cbf789ad0aae
Titia de Castro <laetitia.decastro@obf.ateneo.edu>
2021-03-20 00:38:22 +0800

    created MeetTheTeam.vue

---
+++ src/pages/MeetTheTeam.vue
@@ -0,0 +1,18 @@
+<template>
+  <Layout>
+    
+  </Layout>
+</template>
+
+<script>
+export default {
+  name: "MeetTheTeam",
+  metaInfo: {
+    title: "Meet The Team",
+  }
+}
+</script>
+
+<style>
+
+</style>
